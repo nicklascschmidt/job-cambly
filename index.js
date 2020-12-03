034241c9f18b76a87e9a0fd848232930c1604f85
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2020-12-03 15:04:44 -0500

    added prompt and expected output

---
+++ index.js
@@ -0,0 +1,15 @@
+/** Prompt
+
+Given a string of text, write a program that prints a list of unique words contained in the string
+along with the number of occurrences of each word in the string. The list should be sorted in
+descending order by frequency, and ascending order alphabetically when multiple words occur
+with the same frequency.
+
+expected output:
+  test 3
+  a 2
+  is 2
+  not 1
+  that 1
+  this 1
+*/
