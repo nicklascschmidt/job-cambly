6077380a86cdc428baeafa112763ade8b13698ed
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2020-12-03 15:06:11 -0500

    added basic setup

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ expected output:
   that 1
   this 1
 */
+
+const printUniqueWords = (str) => {
+  
+  return;
+};
+
+const input = 'This is a test. That is not a test. Test';
+const input2 = 'This is a te!@#$st. Th%^&*(at is not a te)[];,./st. Test';
+const input3 = 'test with a hyphen-word. test with under_score. test';
+const inputFinal = 'From the moment the first immigrants arrived on these shores, generations of parents have worked hard and sacrificed whatever is necessary so that their children could have the same chances they had; or the chances they never had. Because while we could never ensure that our children would be rich or successful; while we could never be positive that they would do better than their parents, America is about making it possible to give them the chance. To give every child the opportunity to try. Education is still the foundation of this opportunity. And the most basic building block that holds that foundation together is still reading. At the dawn of the 21st century, in a world where knowledge truly is power and literacy is the skill that unlocks the gates of opportunity and success, we all have a responsibility as parents and librarians, educators and citizens, to instill in our children a love of reading so that we can give them the chance to fulfill their dreams.';
+
+printUniqueWords(input);
